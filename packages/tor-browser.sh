610ba4aa5b098a02a448605adc47f45148b6219a
cerigo-stack <60855309+cerigo-stack@users.noreply.github.com>
2020-02-12 05:03:57 +0200

    Add files via upload

---
+++ packages/tor-browser.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $1/temp
+name="tor-browser" 
+wget https://www.torproject.org/dist/torbrowser/9.0.4/tor-browser-linux64-9.0.4_en-US.tar.xz -O 1.tar.xz
+tar xf 1.tar.xz
+mkdir ../$name
+cp -r tor-browser_en-US/* ../$name/
+
