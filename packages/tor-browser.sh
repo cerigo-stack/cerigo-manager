1437a7c4647d8469ac98875f7e5b738e9ff49e2a
cerigo-stack <60855309+cerigo-stack@users.noreply.github.com>
2020-02-13 04:10:18 +0200

    Update tor-browser.sh

--- packages/tor-browser.sh
+++ packages/tor-browser.sh
@@ -5,5 +5,5 @@ wget https://www.torproject.org/dist/torbrowser/9.0.4/tor-browser-linux64-9.0.4_
 tar xf 1.tar.xz
 mkdir ../$name
 cp -r tor-browser_en-US/* ../$name/
-sudo ln -s ../$name/start-tor-browser.desktop /usr/bin/$name
+sudo ln -s $1/$name/start-tor-browser.desktop /usr/bin/$name
 
